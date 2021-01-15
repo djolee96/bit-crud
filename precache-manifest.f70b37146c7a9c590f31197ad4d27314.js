0927b1de23ccaf9e86478d4cb0cc9483d88e45e2
djolee96 <milan.491989@yahoo.com>
2021-01-15 13:40:10 +0100

    Updates

---
+++ precache-manifest.f70b37146c7a9c590f31197ad4d27314.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01aad30ff50cc229bd2d8038cc1dbe9",
+    "url": "/bit-crud/index.html"
+  },
+  {
+    "revision": "4f4f72901ad7372015aa",
+    "url": "/bit-crud/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "83f600626d88a444b902",
+    "url": "/bit-crud/static/css/main.c635cbb1.chunk.css"
+  },
+  {
+    "revision": "4f4f72901ad7372015aa",
+    "url": "/bit-crud/static/js/2.ac53087f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/bit-crud/static/js/2.ac53087f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83f600626d88a444b902",
+    "url": "/bit-crud/static/js/main.981fdf1e.chunk.js"
+  },
+  {
+    "revision": "388983531f9251045e67",
+    "url": "/bit-crud/static/js/runtime-main.8f9b4141.js"
+  },
+  {
+    "revision": "27a6b4242b21f91e8d8ee5d1fa49991f",
+    "url": "/bit-crud/static/media/lock.27a6b424.jpg"
+  },
+  {
+    "revision": "7084b68f9af769830e136e30ec9949e2",
+    "url": "/bit-crud/static/media/post.7084b68f.png"
+  },
+  {
+    "revision": "557480df79623a4725ccd0d75fa36a94",
+    "url": "/bit-crud/static/media/search.557480df.png"
+  },
+  {
+    "revision": "3edff0c0d9653200f7404198b408c343",
+    "url": "/bit-crud/static/media/userLogo.3edff0c0.png"
+  },
+  {
+    "revision": "bcdb0cac1966079cfb7b05a616a4506c",
+    "url": "/bit-crud/static/media/users.bcdb0cac.svg"
+  }
+]);
